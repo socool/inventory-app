3abdf83fb4658601a9262f1d40d713cd3f7cce16
Kamolsit Mongkolsrisawat <kamolsit@computerlogy.com>
2019-10-14 23:28:24 +0700

    fixed logic product-department

--- src/app/product-department/product-department.component.ts
+++ src/app/product-department/product-department.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-department',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-department.component.sass']
 })
 export class ProductDepartmentComponent implements OnInit {
-
+  @Input() product: Product;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
